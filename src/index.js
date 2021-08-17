686cd0ec6b7d1e4aff6531b3784a3cf7429ddd91
William Mosto <will.mosto@gmail.com>
2021-08-17 18:12:05 -0400

    commit tree e4387764734b7d1924a02fcc43b2836d2677a954

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
-}
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const inputVal = document.getElementById('searchByID').value
+    // console.log(e.target.children[1].value)
+    // console.log(e.target.searchByID.value)
+    // console.log(document.querySelector('input#searchByID').value)
+    form.reset()
 
-document.addEventListener('DOMContentLoaded', init);
+    fetch(`http://localhost:3000/movies/${inputVal}`)
+    .then(res => res.json())
+    .then(data => { 
+        const title = document.querySelector('section#movieDetails h4')
+        const summary = document.querySelector('section#movieDetails p')
+        title.textContent = data.title
+        summary.textContent = data.summary
+        // console.log(data)
+    }) 
+    // .catch(err => alert(err)) 
+  })
+}
+document.addEventListener('DOMContentLoaded', init)
+
